7fd7fd9900bb027869a6f1a1caf186393a9d7b17
tomas <tomasbernardi03@gmail.com>
2025-11-14 15:52:26 -0300

    arreglo

--- src/main/java/org/example/donde_estas/repository/PublicacionRepository.java
+++ src/main/java/org/example/donde_estas/repository/PublicacionRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PublicacionRepository extends JpaRepository<Publicacion, Long> {
-    
+
     Optional<Publicacion> findByMascota(Mascota mascota);
 
     Optional<Publicacion> findById(Long id);
