7266f59f1843ce35b2da72536a2a6b56654183bc
Martiniano Gallo <marfacucosas@gmail.com>
2025-12-16 17:51:36 -0300

    agrego la vista del login y las dependencias

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/register/register.component';
 import { DashboardComponent } from './features/home/dashboard.component';
 import { DetallePublicacionComponent } from './features/publicationDetail/detalle-publicacion.component';
+import { LoginComponent } from './features/login/login.component';
+
 export const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path : 'register', component: RegisterComponent },
-  { path : 'publicacion/:id', component: DetallePublicacionComponent },
-  { path : '**', redirectTo: '' }
+   { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: DashboardComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'publicacion/:id', component: DetallePublicacionComponent },
+  { path: '**', redirectTo: '/login' }
 ];
